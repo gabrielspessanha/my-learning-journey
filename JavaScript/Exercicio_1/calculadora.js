61c33939bef7cb0118eb99d0017ad44aa3785c1b
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-04-28 13:31:56 -0300

    conceitos inicias javascript

---
+++ JavaScript/Exercicio_1/calculadora.js
@@ -0,0 +1,13 @@
+let valor_1 = parseFloat(prompt("digite 1° valor: "))
+let valor_2 = parseFloat(prompt("digite 2° valor: "))
+
+let soma= valor_1 + valor_2
+let subtracao= valor_1 - valor_2
+let multiplicacao= valor_1 * valor_2
+let divisao= valor_1 /valor_2
+
+console.log(
+  `Soma dos valores foi: ${soma}
+  Subtração dos valores foi: ${subtracao}
+  Multiplicação dos valores foi: ${multiplicacao}
+  Divisão dos valores foi: ${divisao}`)

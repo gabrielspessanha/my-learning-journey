b1ee659f118f5e7faadbf77f26f61e4c711ccc94
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-06-06 09:29:02 -0300

    Update calc.js

--- JavaScript - DOM/calc.js/js/calc.js
+++ JavaScript - DOM/calc.js/js/calc.js
@@ -2,17 +2,9 @@ import { calculate } from "./calculate.js"
 import { themeSwitch } from "./theme.js"
 import {eventCopy, eventKey,eventFocus,charkey} from "./eventos.js"
 
-
-const input = document.getElementById('input')
-
 document.querySelectorAll('.charKey').forEach(charkey)
-
 document.getElementById('clear').addEventListener('click',eventFocus)
-
 input.addEventListener('keydown',eventKey )
-
 document.getElementById('equal').addEventListener('click', calculate)
-
 document.getElementById('copyToClipboard').addEventListener('click',eventCopy)
-
 document.getElementById('themeSwitcher').addEventListener('click', themeSwitch)

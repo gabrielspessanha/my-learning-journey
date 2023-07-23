fab7df7a0d438491c3ce760e654427a688b66461
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-07-23 19:40:06 -0300

    Update App.js

--- JavaScript/exercicio_28/App.js
+++ JavaScript/exercicio_28/App.js
@@ -2,12 +2,7 @@ const Imoveis = require("./Imoveis.js")
 
 class App{
 
-  setCreateImovel(house, area, rented){
-    if(house === "Casa"){
-      new Imoveis("Casa",area,rented)
-    }else if(house === "Apartamento"){
-      new Imoveis("Apartamento",area,rented)
-    }else{
+  //222222222222222
       alert("Tipo de apartamento invalido! ")
       return
     }

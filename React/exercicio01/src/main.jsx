d0fd6ed142911de4f971d81518d5bd6db2085c98
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-08-02 15:04:03 -0300

    treinando o JSX

--- React/exercicio01/src/main.jsx
+++ React/exercicio01/src/main.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+document.getElementById('buttonClick').addEventListener('click',()=>{
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+})
+
+

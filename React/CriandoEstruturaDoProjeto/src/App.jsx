4694182c7c7d4a8c1f6ec3011027312d3f60dc50
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-08-02 21:01:27 -0300

    .

--- React/CriandoEstruturaDoProjeto/src/App.jsx
+++ React/CriandoEstruturaDoProjeto/src/App.jsx
@@ -1,3 +1,3 @@
 export function App(){
-  return <h1>hello world</h1>
+  return <h1>hello</h1>
 }
--- React/CriandoEstruturaDoProjeto/src/index.jsx
+++ React/CriandoEstruturaDoProjeto/src/index.jsx
@@ -1,3 +1,4 @@
-import React from "react";
-import { app } from './App'
+import { render }from "react-dom";
+import { App } from './App'
 
+render(<App />, document.getElementById('root'))

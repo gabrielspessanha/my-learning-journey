7ace18f031837871b84a08538640c957451f6bf2
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-08-14 20:24:13 -0300

    novo projeto exercicitando tudo que aprendi até agora no react

---
+++ React/projeto biblioteca de jogos/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react"
+
+
+export default function App(){
+  const [title, setTitle] = useState("")
+  const [cover, setCover] = useState("")
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+    console.log({title,cover})
+    setCover("")
+    setTitle("")
+  }
+
+
+
+
+  return(
+    <div id="app">
+      <h1>Biblioteca de jogos</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Titulo:</label>
+          <input 
+          type="text"
+          name="title" 
+          id="title" 
+          value={title}
+          onChange={(e)=> setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="cover">Capa:</label>
+          <input 
+          type="text"
+          name="cover" 
+          id="cover" 
+          value={cover}
+          onChange={(e)=> setCover(e.target.value)}
+          />
+
+        </div>
+        <button type="Submit">Adicionar a biblioteca</button>
+      </form>
+    </div>
+  )
+}

588c1d8fa60c2f8547bc78d36ec311c21d7a781a
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-08-15 10:13:27 -0300

    finalizando o projeto
    Falta apenas refatorar o código e melhoralo

--- React/projeto biblioteca de jogos/src/App.jsx
+++ React/projeto biblioteca de jogos/src/App.jsx
@@ -2,19 +2,41 @@ import { useState } from "react"
 
 
 export default function App(){
+  const [games, setGames] = useState(()=>{
+    const storageGames = localStorage.getItem("obc-game-lib")
+    if (!storageGames) return []
+    const gameArray = JSON.parse(storageGames)
+    return gameArray
+  })
   const [title, setTitle] = useState("")
   const [cover, setCover] = useState("")
 
-  const handleSubmit = (ev) => {
-    ev.preventDefault()
-    console.log({title,cover})
-    setCover("")
-    setTitle("")
+
+  const addGame = ({ title, cover}) =>{
+    const id = Math.floor(Math.random()* 1000000)
+    const game = {id, title, cover}
+    setGames(state => {
+      const newState =  [...state, game]
+      localStorage.setItem("gb-game-lib", JSON.stringify(newState))
+      return newState
+    })
+  }
+
+  const removeGame = (id) => {
+    setGames(state => {
+      const newState = state.filter(game => game.id!== id)
+      localStorage.setItem("gb-game-lib", JSON.stringify(newState))
+      return newState
+    })
   }
 
 
-
-
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+    addGame({title,cover})
+    setCover("")
+    setTitle("")
+  }
   return(
     <div id="app">
       <h1>Biblioteca de jogos</h1>
@@ -38,10 +60,19 @@ export default function App(){
           value={cover}
           onChange={(e)=> setCover(e.target.value)}
           />
-
         </div>
         <button type="Submit">Adicionar a biblioteca</button>
       </form>
+      <div className="games">
+        {games.map((game) => (
+          <div key={game.id}>
+            <img src={game.cover} alt="Capa do jogo" />
+            <div>
+              <h2>{game.title}</h2>
+              <button onClick={()=> removeGame(game.id)}>Remover</button>
+            </div>
+          </div>))}
+      </div>
     </div>
   )
 }

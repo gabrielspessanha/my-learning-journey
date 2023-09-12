a13ade272f1d963de0613c5475468405083ff391
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-09-12 09:24:57 -0300

    finalizando o exercicio 18 onde faço uso de useState

--- JavaScript-Topicos_avançados/18-exercicio05/src/App.jsx
+++ JavaScript-Topicos_avançados/18-exercicio05/src/App.jsx
@@ -4,32 +4,45 @@ import './styles/global.scss';
 
 function App() {
   const [commenters, setCommenters] = useState([])
+  const [email, setEmail] = useState('')
+  const [comment, setComment] = useState('')
+
+
   function registerComment(ev){
     ev.preventDefault()
 
-    const email = document.querySelector('#email').value
-    const textarea = document.querySelector('#comments').value
+    const date = getCurrentDate()
 
-    const comments ={
+    const newcomments ={
       emailUser: email,
-      date: "12/09/2023",
-      comment: textarea
+      date: date,
+      comment: comment
     }
+    
+    console.log(newcomments)
+    setCommenters([newcomments,...commenters])
+    setEmail('')
+    setComment('')
+  }
 
-    setCommenters(comments)
-    console.log(comments)
+  function getCurrentDate(){
+    const currentDate = new Date()
+    const currentDay = currentDate.getDate();
+    const currentMonth = currentDate.getMonth();
+    const currentYear = currentDate.getFullYear();
 
+    return `${currentDay}/${currentMonth}/${currentYear}`
   }
 
   return (
     <div className='container'>
-      <h2>Seção dde Comentários</h2>
+      <h2>Seção de Comentários</h2>
       <form>
         <label htmlFor="email">Email</label>
-        <input type="text" id="email" />
+        <input type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
 
         <label htmlFor="comments">Comentario</label>
-        <textarea name="comments" id="comments" cols="30" rows="10"></textarea>
+        <textarea name="comments" id="comments" cols="30" rows="10" value={comment} onChange={(e)=> setComment(e.target.value)}></textarea>
 
         <button onClick={registerComment}>
           Enviar Comentário
@@ -38,9 +51,21 @@ function App() {
         <br />
       </form>
 
-      <div className='commenters'>
-        Seja o primeiro a comentar!
-      </div>
+      {commenters.length === 0? (
+        <div className='commenters'>
+          Seja o primeiro a comentar!
+        </div>
+      ): (
+        <ul>
+          {commenters.map( (commenter, index ) => (
+            <li key={index}>
+              <strong>Email:</strong> {commenter.emailUser}<br />
+              <strong>Data:</strong> {commenter.date}<br />
+              <strong>Comentário:</strong> {commenter.comment}<br />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }

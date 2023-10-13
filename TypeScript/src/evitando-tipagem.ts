f602a29e657d6e1c97fad3e913552668278bc8fd
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-10-13 14:15:43 -0300

    conhecendo formas de evitar os tipos

---
+++ TypeScript/src/evitando-tipagem.ts
@@ -0,0 +1,20 @@
+/*
+1°Exemplo:
+
+
+function sendSpaceShip(spaceship: { pilot: string, copilot?: string }) {
+  //...
+}
+
+
+sendSpaceShip( {pilot: "Han Solo", copilot: "chewbacca"})
+
+
+sendSpaceShip( {pilot: 'Luke'})
+
+*/
+
+
+let input = unknown
+
+input = 'test'

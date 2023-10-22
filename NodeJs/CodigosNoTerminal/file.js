3caeb994032f82c410ce6063e4788a84d3551253
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-10-22 17:51:36 -0300

    aprendendo a manipular arquivos

---
+++ NodeJs/CodigosNoTerminal/file.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+fs.writeFile('test.txt', 'Olá NodeJs', err =>{
+  console.log(err)
+})
+
+fs.rename('test.txt', 'test2.txt', err =>{
+  console.log(err)
+})
+
+fs.unlink('test2.txt', err =>{
+  console.log(err)
+})
+
+console.log(__dirname) //caminho atual
